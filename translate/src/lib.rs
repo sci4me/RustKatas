6e53e1b555083b4239c05b3e44aeec22148b6345
sci4me <matthewtpeterson1@gmail.com>
2018-03-14 13:13:31 -0500

    add snake-case translation kata

---
+++ translate/src/lib.rs
@@ -0,0 +1,38 @@
+/// This function accepts a camel-case or title-case name and converts it to snake-case.
+///
+/// # Examples
+/// ```
+/// use translate::translate;
+/// assert_eq!(translate("thisIsACamel"), "this_is_a_camel");
+/// assert_eq!(translate("ThisIsATitle"), "this_is_a_title");
+/// ```
+pub fn translate(s: &str) -> String {
+    let mut result = Vec::new();
+
+    let mut index = 0;
+    for c in s.chars() {
+        let mut cc = c;
+
+        if cc >= 'A' && cc <= 'Z' {
+            if index > 0 {
+                result.push('_');
+            }
+
+            cc = ((cc as u8) + 32) as char; // add 32 because 65 is A and 97 is a and 97 - 65 = 32; this lowercases a capital letter
+        }
+
+        result.push(cc);
+        index += 1;
+    }
+
+    return result.into_iter().collect();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(super::translate("thisIsACamel"), "this_is_a_camel");
+        assert_eq!(super::translate("ThisIsATitle"), "this_is_a_title");
+    }
+}
