9e3724771636f9b56678186f7d8c753c9d103d54
sci4me <matthewtpeterson1@gmail.com>
2018-03-16 09:16:45 -0500

    use borrowing in closest kata to prevent consuming the Vec in the to_zero function

--- closest/src/lib.rs
+++ closest/src/lib.rs
@@ -4,9 +4,9 @@
 ///
 /// # Examples
 /// ```
-/// assert_eq!(closest::to_zero(vec![5, -2, 8, 4, 7, 2]), Some(2));
+/// assert_eq!(closest::to_zero(&vec![5, -2, 8, 4, 7, 2]), Some(2));
 /// ```
-pub fn to_zero(list: Vec<i32>) -> Option<i32> {
+pub fn to_zero(list: &Vec<i32>) -> Option<i32> {
     if list.len() == 0 {
         return None;
     }
@@ -14,7 +14,8 @@ pub fn to_zero(list: Vec<i32>) -> Option<i32> {
     let mut closest : i32 = std::i32::MIN;
     let mut closest_dist : i32 = std::i32::MAX;
 
-    for n in list {
+    for nn in list {
+        let n = *nn;
         let dist = n.abs();
         if dist <= closest_dist {
             if dist == closest_dist && n < 0 { continue; }
@@ -32,37 +33,37 @@ mod tests {
 
     #[test]
     fn test_produces_one_value() {
-        assert_eq!(to_zero(vec![-10]), Some(-10));
+        assert_eq!(to_zero(&vec![-10]), Some(-10));
     }
 
     #[test]
     fn test_produces_smallest_positive() {
-        assert_eq!(to_zero(vec![5, 2, 8, 4, 7, 1]), Some(1));
+        assert_eq!(to_zero(&vec![5, 2, 8, 4, 7, 1]), Some(1));
     }
 
     #[test]
     fn test_produces_no_result() {
-        assert_eq!(to_zero(vec![]), None);
+        assert_eq!(to_zero(&vec![]), None);
     }
 
     #[test]
     fn test_produces_greatest_negative() {
-        assert_eq!(to_zero(vec![-5, -2, -8, -4, -7, -1]), Some(-1));
+        assert_eq!(to_zero(&vec![-5, -2, -8, -4, -7, -1]), Some(-1));
     }
 
     #[test]
     fn test_produces_negative() {
-        assert_eq!(to_zero(vec![5, 2, 8, 4, 7, -1]), Some(-1));
+        assert_eq!(to_zero(&vec![5, 2, 8, 4, 7, -1]), Some(-1));
     }
 
     #[test]
     fn test_produces_positive() {
-        assert_eq!(to_zero(vec![-5, -2, -8, -4, -7, 1]), Some(1));
+        assert_eq!(to_zero(&vec![-5, -2, -8, -4, -7, 1]), Some(1));
     }    
 
     #[test]
     fn test_favor_positive() {
-        assert_eq!(to_zero(vec![5, -2, 8, 4, 7, 2]), Some(2));
-        assert_eq!(to_zero(vec![5, 2, 8, 4, 7, -2]), Some(2));
+        assert_eq!(to_zero(&vec![5, -2, 8, 4, 7, 2]), Some(2));
+        assert_eq!(to_zero(&vec![5, 2, 8, 4, 7, -2]), Some(2));
     }
 }
