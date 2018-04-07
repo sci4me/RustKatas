a94a449d349c1bae90c901002a45cdcdd6b5eb19
sci4me <matthewtpeterson1@gmail.com>
2018-04-06 22:20:21 -0500

    implement fizz buzz

---
+++ fizz_buzz/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+	for i in 1..101 {
+		if i % 3 == 0 && i % 5 == 0 {
+			print!("FizzBuzz ");
+		} else if i % 3 == 0 {
+			print!("Fizz ");
+		} else if i % 5 == 0 {
+			print!("Buzz ");
+		} else {
+			print!("{} ", i);
+		}
+	}
+	print!("\n");
+}
